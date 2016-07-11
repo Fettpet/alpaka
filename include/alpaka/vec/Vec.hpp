920ed9dd2e336046e563b5f0c05b12b13c75c20e
Benjamin Worpitz <benjaminworpitz@gmail.com>
2016-07-11 21:20:30 +0200

    Only allow Vec creation with correct types

--- include/alpaka/vec/Vec.hpp
+++ include/alpaka/vec/Vec.hpp
@@ -30,6 +30,7 @@
 
 #include <alpaka/meta/IntegerSequence.hpp>  // meta::MakeIntegerSequence
 #include <alpaka/meta/Fold.hpp>             // meta::foldr
+#include <alpaka/meta/Metafunctions.hpp>    // meta::Conjunction
 #include <alpaka/core/Align.hpp>            // ALPAKA_OPTIMAL_ALIGNMENT_SIZE
 #include <alpaka/core/Assert.hpp>           // assertValueUnsigned
 #include <alpaka/core/Common.hpp>           // ALPAKA_FN_HOST_ACC
@@ -181,8 +182,19 @@ namespace alpaka
                 // There have to be dim arguments.
                 (sizeof...(TArgs)+1 == TDim::value)
                 &&
-                (std::is_same<TSize, typename std::decay<TArg0>::type>::value)
-                >::type>
+                // They all have to have type TSize
+                meta::Conjunction<
+                    std::true_type,
+                    std::is_same<
+                        typename std::decay<TArg0>::type,
+                        TSize
+                    >,
+                    std::is_same<
+                        typename std::decay<TArgs>::type,
+                        TSize
+                    >...
+                >::value
+            >::type>
         ALPAKA_FN_HOST_ACC Vec(
             TArg0 && arg0,
             TArgs && ... args) :
